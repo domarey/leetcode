886a40041df40e1696561d66b7c2e87cb3d0f32b
Domenico Manalo <domenico.manalo@gmail.com>
2024-02-03 18:00:03 +0100

    Update solution.py

--- solutions/3025. Find the Number of Ways to Place People I/solution.py
+++ solutions/3025. Find the Number of Ways to Place People I/solution.py
@@ -4,11 +4,14 @@ class Solution(object):
         :type points: List[List[int]]
         :rtype: int
         """
+        # order points by the first coordinate (x), and then by the second (y)
         orderedPoints = sorted(points, key=lambda k: (k[0], -k[1]))
         count = 0
         for i in range(len(orderedPoints)-1):
             for j in range(i+1,len(orderedPoints)):
                 if orderedPoints[i][1] >= orderedPoints[j][1]:
+                        # if you are here, the two points might be a good fit
+                        # need to check if there are other points inside the rectangle
                         if j-i >  1:
                             pointInside = False
                             for k in range(i+1,j):
