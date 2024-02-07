5e0210d91bcc2b944812e56528e8e514dcb8a4f0
Domenico Manalo <domenico.manalo@gmail.com>
2024-02-07 13:13:04 +0100

    Update solution.py

--- solutions/0387. First Unique Character in a String/solution.py
+++ solutions/0387. First Unique Character in a String/solution.py
@@ -1,1 +1,16 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3.7
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        alphabet = {}
+        for c in s:
+            if c in alphabet.keys():
+                alphabet[c] += 1
+            else:
+                alphabet[c] = 1
+
+        # The standard Python dict preserves the insertion order in Python 3.7+
+        for c in alphabet.keys():
+            if alphabet[c] == 1:
+                return s.index(c)
+        return -1
